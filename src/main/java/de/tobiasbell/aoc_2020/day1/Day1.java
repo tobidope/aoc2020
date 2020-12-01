e728e12e9a1e4aebaa097734ae7d0d2885b5b571
Tobias Bell <tobias.bell@gmail.com>
2020-12-01 22:48:31 +0100

    Day 1 solved

--- src/main/java/de/tobiasbell/aoc_2020/day1/Day1.java
+++ src/main/java/de/tobiasbell/aoc_2020/day1/Day1.java
@@ -1,12 +1,57 @@
 package de.tobiasbell.aoc_2020.day1;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 public class Day1 {
 
+    public static final int EXPECTED = 2020;
+
+    public static long[] pairFinder(List<Long> numbers) {
+        SortedSet<Long> numberSet = new TreeSet<>(numbers);
+        for (var n : numberSet) {
+            if (n > EXPECTED) {
+                continue;
+            }
+            final long l = EXPECTED - n;
+            final SortedSet<Long> lesser = numberSet.tailSet(n);
+            if (lesser.contains(l)) {
+                return new long[]{n, l};
+            }
+
+        }
+        return new long[0];
+    }
+
+    public static long[] findTriple(List<Long> numbers) {
+        SortedSet<Long> set = new TreeSet<>(numbers);
+        for (var first : set) {
+            final SortedSet<Long> rest = set.tailSet(first);
+            for (var second : rest) {
+                final SortedSet<Long> rest2 = rest.tailSet(second);
+                final long l = EXPECTED - first - second;
+                if (rest2.contains(l)) {
+                    return new long[]{first, second, l};
+                }
+            }
+        }
+        return new long[0];
+    }
+
     public static long solve1(final String input) {
-        return 0;
+        final long[] longs = pairFinder(Arrays.stream(input.split("\\R"))
+                .map(Long::parseLong)
+                .collect(Collectors.toList()));
+        return longs[0] * longs[1];
     }
 
     public static long solve2(String input) {
-        return 0;
+        final long[] longs = findTriple(Arrays.stream(input.split("\\R"))
+                .map(Long::parseLong)
+                .collect(Collectors.toList()));
+        return longs[0] * longs[1] * longs[2];
     }
 }
--- src/test/java/de/tobiasbell/aoc_2020/day1/Day1Test.java
+++ src/test/java/de/tobiasbell/aoc_2020/day1/Day1Test.java
@@ -4,18 +4,37 @@ import de.tobiasbell.aoc_2020.util.InputReader;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Day1Test {
 
     @Test
+    void pairFinder() throws IOException {
+        var test = """
+                1721
+                979
+                366
+                299
+                675
+                1456
+                """;
+        final List<Long> numbers = Stream.of(test.split("\\R"))
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+        assertThat(Day1.pairFinder(numbers)).containsExactlyInAnyOrder(1721, 299);
+    }
+
+    @Test
     void solve1() throws IOException {
-        assertThat(Day1.solve1(InputReader.getInput(1))).isEqualTo(0);
+        assertThat(Day1.solve1(InputReader.getInput(1))).isEqualTo(567171L);
     }
 
     @Test
     void solve2() throws IOException {
-        assertThat(Day1.solve2(InputReader.getInput(1))).isEqualTo(0);
+        assertThat(Day1.solve2(InputReader.getInput(1))).isEqualTo(212428694L);
     }
 }
