59e8be19fdb568ba5ddd8c6eca5e77ecd01d970a
Tobias Bell <tobias.bell@gmail.com>
2020-12-03 18:41:52 +0100

    Day 3

--- src/main/java/de/tobiasbell/aoc_2020/Day3.java
+++ src/main/java/de/tobiasbell/aoc_2020/Day3.java
@@ -1,11 +1,54 @@
 package de.tobiasbell.aoc_2020;
 
+import de.tobiasbell.aoc_2020.util.InputReader;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Day3 {
     public static long solve1(final String input) {
-        return 0;
+        final List<String> list = InputReader.lines(input).collect(Collectors.toList());
+        final TreeMap map = new TreeMap(list);
+        return visitedTrees(map, 3, 1);
     }
 
     public static long solve2(String input) {
-        return 0;
+        final List<String> list = InputReader.lines(input).collect(Collectors.toList());
+        final TreeMap map = new TreeMap(list);
+        return visitedTrees(map, 1, 1)
+                * visitedTrees(map, 3, 1)
+                * visitedTrees(map, 5, 1)
+                * visitedTrees(map, 7, 1)
+                * visitedTrees(map, 1, 2);
+    }
+
+    public static long visitedTrees(final TreeMap map, int right, int down) {
+        int x = 0;
+        int y = 0;
+        long trees = 0;
+        do {
+            x += down;
+            y += right;
+            if (map.hasTree(x, y)) {
+                trees++;
+            }
+        } while (x < map.rows() - 1);
+        return trees;
+    }
+
+    public static record TreeMap(List<String> map) {
+        public static char TREE = '#';
+
+        public boolean hasTree(int x, int y) {
+            return map.get(x).charAt(y % columns()) == TREE;
+        }
+
+        public int rows() {
+            return map.size();
+        }
+
+        public int columns() {
+            return map.get(0).length();
+        }
     }
 }
--- src/test/java/de/tobiasbell/aoc_2020/Day3Test.java
+++ src/test/java/de/tobiasbell/aoc_2020/Day3Test.java
@@ -10,11 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class Day3Test {
     @Test
     void solve1() throws IOException {
-        assertThat(Day3.solve1(InputReader.getInput(3))).isEqualTo(0);
+        assertThat(Day3.solve1(InputReader.getInput(3))).isEqualTo(259);
     }
 
     @Test
     void solve2() throws IOException {
-        assertThat(Day3.solve2(InputReader.getInput(3))).isEqualTo(0);
+        assertThat(Day3.solve2(InputReader.getInput(3))).isEqualTo(2_224_913_600L);
     }
 }
