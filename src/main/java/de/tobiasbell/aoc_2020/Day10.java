144bb1171c09a3013646bde67387cf90a69dd7fd
Tobias Bell <tobias.bell@gmail.com>
2020-12-12 13:56:53 +0100

    Solved Day 10

--- src/main/java/de/tobiasbell/aoc_2020/Day10.java
+++ src/main/java/de/tobiasbell/aoc_2020/Day10.java
@@ -1,14 +1,59 @@
 package de.tobiasbell.aoc_2020;
 
+import de.tobiasbell.aoc_2020.util.Input;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Day10 {
     private Day10() {
     }
 
+    public static List<Long> parseAdaptersSorted(final String input) {
+        return Input.lines(input)
+                .map(Long::parseLong)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public static Map<Long, Long> joltDifferenceDistribution(final Collection<Long> adapters) {
+        var currentJolt = 0L;
+        var map = new HashMap<Long, Long>();
+        for (var adapter : adapters) {
+            final long difference = adapter - currentJolt;
+            currentJolt = adapter;
+            map.merge(difference, 1L, Long::sum);
+        }
+        map.merge(3L, 1L, Long::sum);
+        return map;
+    }
+
+    public static long arrangements(final List<Long> adapters) {
+        Map<Long, Long> countMap = new HashMap<>();
+        countMap.put(0L, 1L);
+        for (var adapter :
+                adapters) {
+            final long sum =
+                    countMap.getOrDefault(adapter - 1, 0L)
+                            + countMap.getOrDefault(adapter - 2, 0L)
+                            + countMap.getOrDefault(adapter - 3, 0L);
+            countMap.put(adapter, sum);
+        }
+        return countMap.get(adapters.get(adapters.size() - 1));
+    }
+
     public static long solve1(String puzzle) {
-        return 0;
+        final Collection<Long> adaptersSorted = parseAdaptersSorted(puzzle);
+        final Map<Long, Long> distribution = joltDifferenceDistribution(adaptersSorted);
+        return distribution.get(1L) * distribution.get(3L);
     }
 
     public static long solve2(String puzzle) {
-        return 0;
+        final List<Long> adaptersSorted = parseAdaptersSorted(puzzle);
+        return arrangements(adaptersSorted);
+
     }
 }
--- src/test/java/de/tobiasbell/aoc_2020/Day10Test.java
+++ src/test/java/de/tobiasbell/aoc_2020/Day10Test.java
@@ -3,31 +3,132 @@ package de.tobiasbell.aoc_2020;
 import de.tobiasbell.aoc_2020.util.Input;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 class Day10Test {
 
     @Test
+    void joltDifferenceDistribution() {
+        // given
+        var example = """
+                1
+                4
+                5
+                6
+                7
+                10
+                11
+                12
+                15
+                16
+                19
+                """;
+        var adapters = Day10.parseAdaptersSorted(example);
+        // when
+        final Map<Long, Long> distribution = Day10.joltDifferenceDistribution(adapters);
+        //then
+        assertThat(distribution)
+                .containsKeys(1L, 3L)
+                .contains(
+                        entry(1L, 7L),
+                        entry(3L, 5L)
+                );
+    }
+
+    @Test
+    void joltDifferenceDistributionBigger() {
+        // given
+        var example = """
+                28
+                33
+                18
+                42
+                31
+                14
+                46
+                20
+                48
+                47
+                24
+                23
+                49
+                45
+                19
+                38
+                39
+                11
+                1
+                32
+                25
+                35
+                8
+                17
+                7
+                9
+                4
+                2
+                34
+                10
+                3
+                """;
+        var adapters = Day10.parseAdaptersSorted(example);
+        // when
+        final Map<Long, Long> distribution = Day10.joltDifferenceDistribution(adapters);
+        //then
+        assertThat(distribution)
+                .contains(
+                        entry(1L, 22L),
+                        entry(3L, 10L)
+                );
+    }
+
+
+    @Test
     void solve1() {
         // given
-        var example = """                
+        var example = """
+                16
+                10
+                15
+                5
+                1
+                11
+                7
+                19
+                6
+                12
+                4              
                 """;
         // when
         final long result = Day10.solve1(example);
         //then
-        assertThat(result).isEqualTo(0);
-        assertThat(Day10.solve1(Input.puzzleInput(10))).isEqualTo(0);
+        assertThat(result).isEqualTo(35);
+        assertThat(Day10.solve1(Input.puzzleInput(10))).isEqualTo(1856L);
     }
 
     @Test
     void solve2() {
         // given
-        var example = """          
+        var example = """
+                1
+                4
+                5
+                6
+                7
+                10
+                11
+                12
+                15
+                16
+                19
                 """;
         // when
         final long result = Day10.solve2(example);
         //then
-        assertThat(result).isEqualTo(0);
-        assertThat(Day10.solve2(Input.puzzleInput(10))).isEqualTo(0);
+        assertThat(result).isEqualTo(8);
+        assertThat(Day10.solve2(Input.puzzleInput(10))).isEqualTo(2314037239808L);
     }
 }
